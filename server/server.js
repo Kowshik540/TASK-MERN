e2a042e5ca6843f07dcfd0056dad494504a4d671
Thota kowshik <kowshikthota43@gmail.com>
2026-05-24 15:49:17 +0530

    Update CORS middleware to allow specific origins

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ const statsRoutes = require('./routes/stats');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://task-mern-ruddy.vercel.app'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
